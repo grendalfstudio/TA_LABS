983c063fda94c9b8ca26d4f1a841b5db1557d660
AndrryArmor <andrry1@gmail.com>
2019-05-22 02:24:01 +0300

    Created new project for css style aplying

---
+++ CssStyleAplier/CssStyleAplier/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace CssStyleAplier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.radioButton = new System.Windows.Forms.RadioButton();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(12, 12);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(200, 50);
+            this.button.TabIndex = 0;
+            this.button.Text = "Button";
+            this.button.UseVisualStyleBackColor = true;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(9, 65);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(33, 13);
+            this.label.TabIndex = 1;
+            this.label.Text = "Label";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 81);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(200, 20);
+            this.textBox.TabIndex = 2;
+            this.textBox.Text = "TextBox";
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Location = new System.Drawing.Point(12, 107);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(75, 17);
+            this.checkBox.TabIndex = 3;
+            this.checkBox.Text = "CheckBox";
+            this.checkBox.UseVisualStyleBackColor = true;
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "Button",
+            "Label",
+            "TextBox",
+            "CheckBox",
+            "ComboBox",
+            "RadioButton",
+            "ListBox"});
+            this.comboBox.Location = new System.Drawing.Point(12, 128);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(200, 21);
+            this.comboBox.TabIndex = 4;
+            this.comboBox.Text = "ComboBox";
+            // 
+            // radioButton
+            // 
+            this.radioButton.AutoSize = true;
+            this.radioButton.Location = new System.Drawing.Point(12, 155);
+            this.radioButton.Name = "radioButton";
+            this.radioButton.Size = new System.Drawing.Size(84, 17);
+            this.radioButton.TabIndex = 5;
+            this.radioButton.TabStop = true;
+            this.radioButton.Text = "RadioButton";
+            this.radioButton.UseVisualStyleBackColor = true;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Items.AddRange(new object[] {
+            "Button",
+            "Label",
+            "TextBox",
+            "CheckBox",
+            "ComboBox",
+            "RadioButton",
+            "ListBox"});
+            this.listBox.Location = new System.Drawing.Point(12, 178);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(200, 69);
+            this.listBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 266);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.radioButton);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            this.Text = "CSS Style Aplier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.RadioButton radioButton;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
